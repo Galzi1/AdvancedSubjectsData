e9c2a531c9eb36753c3a95ec47277885fb677e08
GalZi <galzi123@gmail.com>
2020-02-02 22:44:08 +0200

    Simple ctree
    Simple ctree, with confusion matrix.

--- ffp_solution.R
+++ ffp_solution.R
@@ -1,1 +1,18 @@
-setwd("")
+setwd("D:/ть/RProgramming/AdvancedSubjectsData")
+
+traindata.df <- read.csv("ffp_train.csv")
+
+# partition the data
+set.seed(1)
+train.index <- sample(1:dim(traindata.df)[1], dim(traindata.df)*0.6)
+train.df <- traindata.df[train.index, ]
+valid.df <- traindata.df[-train.index, ]
+
+# classification tree
+library(party)
+tr <- ctree(BUYER_FLAG ~ ., data = train.df)
+plot(tr)
+pred <- predict(tr, valid.df)
+
+confusionMatrix(as.factor(ifelse(pred>=0.5, 1, 0)), as.factor(valid.df$BUYER_FLAG))
+
